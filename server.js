9d1408e3110fea4a3ffd662878519606afd29945
William Martinez <wemz17@gmail.com>
2016-09-28 20:41:26 -0400

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+var express =  require("express");
+var logfmt  =  require("logfmt");
+
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/',function(req,res){
+	res.send('hola desde nodejs');
+})
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port,function(){
+	console.log(" escuchando en el puerto"+port)
+})
